6b530c0dcc3f1ba751d3dbe1ec7ff9a730d43cbb
Mohit Adhikari <143146421+mohitdebugs@users.noreply.github.com>
2023-10-06 20:40:47 +0530

    Create 06-10-2023 | 343 Integer Break.cpp

---
+++ 10 October-2023/06-10-2023 | 343 Integer Break.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int count = n / 3;
+        int remainder = n % 3;
+        
+        if (remainder == 0)
+            return pow(3, count);
+        else if (remainder == 1)
+            return pow(3, count-1)*4;
+        else
+            return pow(3, count)*2;
+    }
+};
